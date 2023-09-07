c485968b647dc5597a764b04e9c96b4fc510ac29
Ali <alihuss1017@gmail.com>
2023-09-06 17:12:51 -0700

    [ADD] saved NN model

---
+++ models/model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+class CustomModel(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2):
+        super(CustomModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size2, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+        return x
